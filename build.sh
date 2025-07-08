8ebe95d33e80b2af2645b2b3fd2e3b99167d8ca9
Hiren Patel <hirenpatel1198@gmail.com>
2025-07-08 19:17:27 +0530

    Installed whitenoise and updated requirements.txt

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 # exit on error
 set -o errexit
 
-pip install -r requirements_render.txt
+pip install -r requirements_minimal.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
